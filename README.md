777d5d172ddfaac8c947129d9331a0332f005e8a
Omkar Arora <aroraomkar@gmail.com>
2021-10-07 15:49:20 +0530

    feat: Express router setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rock Paper Scissors
+
+This is the backend for the game
+
+App Link- TBD
---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Connected to Rock-Paper-Scissors server");
+});
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
+  console.log("Server started on port: ", PORT);
+});
